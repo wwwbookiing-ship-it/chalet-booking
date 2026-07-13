df3213727fed29b4f36c5c28fd8ae2a139418010
wwwbookiing-ship-it <www.bookiing@gmail.com>
2026-07-13 08:30:42 +0300

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,25 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
 
+    if (url.pathname === "/api/status") {
+      return new Response(
+        JSON.stringify({
+          success: true,
+          message: "Worker is running"
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+          }
+        }
+      );
+    }
+
+    return new Response("Chalet Booking Worker is running", {
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8"
+      }
+    });
+  }
+};
